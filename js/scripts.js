43a179587dc2cd485bf617f26942e25e56936eb6
root <alessandro.cpr@gmail.com>
2016-03-17 00:11:40 +0100

    Quasi finito implementazione form preventivi e fornitori

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,53 @@
-/**
- * Created by ale on 15/03/16.
- */
+$("#condominiForm").validator().on("submit", function (event) {
+    if (event.isDefaultPrevented()) {
+        // handle the invalid form...
+        formError();
+        submitMSG(false, "Hai completato correttamente i campi?");
+    } else {
+        // everything looks good!
+        event.preventDefault();
+        submitForm();
+    }
+});
+
+
+function submitForm(){
+    // Initiate Variables With Form Content
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
+
+    $.ajax({
+        type: "POST",
+        url: "php/form-process.php",
+        data: "name=" + name + "&email=" + email + "&message=" + message,
+        success : function(text){
+            if (text == "success"){
+                formSuccess();
+            } else {
+                formError();
+                submitMSG(false,text);
+            }
+        }
+    });
+}
+
+function formSuccess(){
+    $("#condominiForm")[0].reset();
+    submitMSG(true, "Messaggio inviato!")
+}
+
+function formError(){
+    $("#condominiForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+        $(this).removeClass();
+    });
+}
+
+function submitMSG(valid, msg){
+    if(valid){
+        var msgClasses = "h3 text-center tada animated text-success";
+    } else {
+        var msgClasses = "h3 text-center text-danger";
+    }
+    $("#msgSubmitCondomini").removeClass().addClass(msgClasses).text(msg);
+}
